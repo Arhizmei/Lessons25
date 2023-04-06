279979fede1f273f8e619556b6616a50ddbe8204
arhizmei <arhizmei@gmail.com>
2023-04-06 15:21:10 +0300

    1.1

---
+++ app/src/main/java/com/example/lessons25/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.lessons25
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun onClick (view: View){
+        val i= Intent()
+        i.putExtra("key1", "done")
+        setResult(RESULT_OK, i)
+        finish()
+
+
+
+    }
+}
